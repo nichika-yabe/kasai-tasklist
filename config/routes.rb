2b7685f60b4e5d9f907fd37ce7b0671788046aee
nichika.yabe <nichika20000217@gmail.com>
2020-08-06 04:17:55 +0000

    tasklist-kadi

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  root to: "tasks#index"
+  root to: "toppages#index"
   
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:new, :create]
+  
+  resources :tasks, only: [:create, :destroy]
   resources :tasks
 end
